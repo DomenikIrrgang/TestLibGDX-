c572ee5093c85c717074ea6f0e323ea8608221ba
DomenikSTUDIOS <domenikirrgang@web.de>
2014-01-31 21:36:22 +0100

    upload initial project

---
+++ ProjectXXX-desktop/src/com/DCStudios/ProjectXXX/Main.java
@@ -0,0 +1,16 @@
+package com.DCStudios.ProjectXXX;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "ProjectXXX";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new ProjectXXX(), cfg);
+	}
+}
